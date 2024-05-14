74fb94c2c75451ab0cb8ebe014d59df241e8c71f
272Jayden <jaydenfokkink@gmail.com>
2024-05-14 13:29:10 +0200

    added folders and an item ig

---
+++ ChaoticDetectives/Assets/_Project/Scripts/Collectables/Items.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Items : MonoBehaviour, IInteractable
+{
+    [SerializeField] private List<bool> objectives = new List<bool>();
+
+    public void OnClick()
+    {
+        if(objectives.Count > 0)
+        {
+            for(int i = 0; i < objectives.Count; i++)
+            {
+                if (objectives[i] == false) 
+                {
+                    break;
+                } else if (objectives[i] == true && i == objectives.Count -1)
+                {
+                    Debug.Log("Clicked!");
+                }
+                
+            }
+        }
+    }
+
+    public void OnHoverEnter()
+    {
+        Debug.Log("Hovering!");
+    }
+
+    public void OnHoverExit()
+    {
+        Debug.Log("Not hovering!");
+    }
+}
