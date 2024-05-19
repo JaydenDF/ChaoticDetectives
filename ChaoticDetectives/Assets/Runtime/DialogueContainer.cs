a1b45cb21c8855e614e599372ea99c35d9a8dafa
AlexVeraPons <Alexandroverapons@gmail.com>
2024-05-19 16:08:03 +0200

    getting dialogue now works pending implementing the actual visual system

--- ChaoticDetectives/Assets/Runtime/DialogueContainer.cs
+++ ChaoticDetectives/Assets/Runtime/DialogueContainer.cs
@@ -13,23 +13,65 @@ public class DialogueContainer : ScriptableObject
         // Find the target node GUID connected to the port "Next"
         string firstNodeGuid = NodeLinks.Find(x => x.PortName == "Next").TargetNodeGuid;
 
-        // Find the dialogue text for the target node GUID
-        string dialogueText = DialogueNodeData.Find(x => x.NodeGUID == firstNodeGuid).DialogueText;
+        string dialogueText = GetDialogueText(firstNodeGuid);
+        string[] responses = GetResponses(firstNodeGuid);
 
-        // Find the responses for the target node GUID
+        return new DialogueStep(dialogueText, responses);
+    }
+
+    public DialogueStep GetNextStepFromIndex(DialogueStep previousDialogue, int index)
+    {
+        //get guid with the previous dialogue text
+        string previousNodeGuid = DialogueNodeData.Find(x => x.DialogueText == previousDialogue.DialogueText).NodeGUID;
+
+        List<string> targetNodeGuids = new List<string>();
+
+        foreach (NodeLinkData nodeLink in NodeLinks)
+        {
+            if (nodeLink.BaseNodeGuid == previousNodeGuid)
+            {
+                targetNodeGuids.Add(nodeLink.TargetNodeGuid);
+            }
+        }
+
+        string nextNodeGuid = targetNodeGuids[index];
+
+        string dialogueText = GetDialogueText(nextNodeGuid);
+        string[] responses = GetResponses(nextNodeGuid);
+
+        DialogueStep nextDialogue = new DialogueStep(dialogueText, new string[0]);
+        return nextDialogue;
+    }
+
+    public DialogueStep GetNextStepFromResponse(DialogueStep previousDialogue, string response)
+    {
+        //get guid with the previous dialogue text
+        string previousNodeGuid = DialogueNodeData.Find(x => x.DialogueText == previousDialogue.DialogueText).NodeGUID;
+
+        string nextNodeGuid = NodeLinks.Find(x => x.BaseNodeGuid == previousNodeGuid && DialogueNodeData.Find(y => y.NodeGUID == x.TargetNodeGuid).DialogueText == response).TargetNodeGuid;
+
+        string dialogueText = GetDialogueText(nextNodeGuid);
+        string[] responses = GetResponses(nextNodeGuid);
+
+        DialogueStep nextDialogue = new DialogueStep(dialogueText, responses);
+        return nextDialogue;
+    }
+
+    private string[] GetResponses(string nodeGuid)
+    {
         List<string> responses = new List<string>();
         foreach (NodeLinkData nodeLink in NodeLinks)
         {
-            if (nodeLink.BaseNodeGuid == firstNodeGuid)
+            if (nodeLink.BaseNodeGuid == nodeGuid)
             {
                 responses.Add(DialogueNodeData.Find(x => x.NodeGUID == nodeLink.TargetNodeGuid).DialogueText);
             }
         }
-
-
-        
-
-        return new DialogueStep(dialogueText, responses.ToArray());
+        return responses.ToArray();
     }
 
+    private string GetDialogueText(string nodeGuid)
+    {
+        return DialogueNodeData.Find(x => x.NodeGUID == nodeGuid).DialogueText;
+    }
 }
