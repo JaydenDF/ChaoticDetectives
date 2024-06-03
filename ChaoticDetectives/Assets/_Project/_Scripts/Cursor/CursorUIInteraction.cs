52da5febc4189c8d2c6a79b7a7848a789c4a4bc4
AlexVeraPons <Alexandroverapons@gmail.com>
2024-06-03 03:05:44 +0200

    sprint 4 fks

--- ChaoticDetectives/Assets/_Project/_Scripts/Cursor/CursorUIInteraction.cs
+++ ChaoticDetectives/Assets/_Project/_Scripts/Cursor/CursorUIInteraction.cs
@@ -1,8 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor.Tilemaps;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CursorUIInteraction : MonoBehaviour, IInteractable
 {
