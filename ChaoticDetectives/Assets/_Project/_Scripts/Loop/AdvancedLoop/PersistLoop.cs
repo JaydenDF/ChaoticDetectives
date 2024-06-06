6943fc821d5e455365af036a3a7c850f9a2a4a87
AlexVeraPons <Alexandroverapons@gmail.com>
2024-06-06 11:26:36 +0200

    Loop manager update

--- ChaoticDetectives/Assets/_Project/_Scripts/Loop/AdvancedLoop/PersistLoop.cs
+++ ChaoticDetectives/Assets/_Project/_Scripts/Loop/AdvancedLoop/PersistLoop.cs
@@ -1,12 +1,18 @@
 using UnityEngine;
 
-public class PersistLoop : MonoBehaviour, ILoop{
+public class PersistLoop : MonoBehaviour, ILoop
+{
     public bool persist = false;
 
     public void Loop(Transform targetLoop)
     {
-        if(persist == false) {return;}
+        if (persist == false) { return; }
 
         this.transform.parent = targetLoop;
+    }
+
+    public void persistLoop(bool condition)
+    {
+        persist = condition;
     }
 }
