ae9a762dd79b85d09c8cd9cf513a5ffc5253c067
272Jayden <jaydenfokkink@gmail.com>
2024-11-19 16:01:27 +0100

    progress bar done

--- ChaoticDetectives/Assets/_Project/_Scripts/Animation/IvyAnimation.cs
+++ ChaoticDetectives/Assets/_Project/_Scripts/Animation/IvyAnimation.cs
@@ -5,32 +5,42 @@ using UnityEngine;
 public class IvyAnimation : MonoBehaviour
 {
     private Animator _animator;
+    bool hasBeenPlayed = false;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
-        StartCoroutine(changeIvySprite(1f));
+        changeIvySprite();
     }
+
+    private void Update()
+    {
+        if (!hasBeenPlayed)
+        {
+            StartCoroutine(changeIvySprite(1f));
+        }
+    }
+
     private IEnumerator changeIvySprite(float delay = 2)
     {
         if (_animator != null)
         {
-            
+            ResetIvyAni();
             yield return new WaitForSeconds(delay);
             IvySeesYou();
+            hasBeenPlayed = true;
         }
     }
 
     private void IvySeesYou()
     {
-        _animator.SetBool("NeedsToReset", false);
         _animator.SetBool("IvySeesYou", true);
     }
 
     public void ResetIvyAni()
     {
         _animator.SetBool("IvySeesYou", false);
-        _animator.SetBool("NeedsToReset", true);
+        hasBeenPlayed = false;
     }
 
     private void OnDisable()
